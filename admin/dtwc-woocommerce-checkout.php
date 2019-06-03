a0b603456e262e5f9556b2643ea736837a5e1622
Robert DeVore <deviodigital@gmail.com>
2019-06-02 22:23:49 -0400

    :package: NEW: Delivery time select option on checkout

--- admin/dtwc-woocommerce-checkout.php
+++ admin/dtwc-woocommerce-checkout.php
@@ -11,11 +11,11 @@
  */
 
 /**
- * Add Delivery Date checkout field
+ * Add Delivery Date & Time checkout fields
  * 
  * @since 1.0
  */
-function dtwc_delivery_date_checkout_field( $checkout ) {
+function dtwc_delivery_info_checkout_fields( $checkout ) {
 
     // Create Delivery date field.
     woocommerce_form_field( 'dtwc_delivery_date', array(
@@ -24,11 +24,27 @@ function dtwc_delivery_date_checkout_field( $checkout ) {
         'label'    => __( 'Delivery date', 'dtwc' ),
         'required' => true,
     ), $checkout->get_value( 'dtwc_delivery_date' ) );
+
+    // Create Delivery time field.
+    woocommerce_form_field( 'dtwc_delivery_time', array(
+        'type'     => 'select',
+        'class'    => array( 'dtwc_delivery_time form-row-wide' ),
+        'label'    => __( 'Delivery time', 'dtwc' ),
+        'required' => true,
+        'options'  => array(
+            ''         => 'Select a time',
+            '9am-12pm' => '9am - 12pm',
+            '12pm-3pm' => '12pm - 3pm',
+            '3pm-6pm'  => '3pm - 6pm',
+            '6pm-9pm'  => '6pm - 9pm',
+        )
+    ), $checkout->get_value( 'dtwc_delivery_time' ) );
+
 }
-add_action( 'woocommerce_after_checkout_billing_form', 'dtwc_delivery_date_checkout_field' , 10, 1 );
+add_action( 'woocommerce_after_checkout_billing_form', 'dtwc_delivery_info_checkout_fields' , 10, 1 );
 
 /**
- * Process the Delivery Date checkout field
+ * Process the Delivery Date & Time checkout fields
  * 
  * @since 1.0
  */
@@ -37,27 +53,34 @@ function dtwc_delivery_date_checkout_field_process() {
     if ( ! $_POST['dtwc_delivery_date'] ) {
         wc_add_notice( __( 'Please enter a delivery date.', 'dtwc' ), 'error' );
     }
+    // Check if set, if its not set add an error.
+    if ( ! $_POST['dtwc_delivery_time'] ) {
+        wc_add_notice( __( 'Please select a preferred delivery time.', 'dtwc' ), 'error' );
+    }
 }
 add_action( 'woocommerce_checkout_process', 'dtwc_delivery_date_checkout_field_process' );
 
 /**
- * Save Delivery Date checkout field
+ * Save Delivery Date & Time checkout fields
  * 
  * @since 1.0
  */
-function dtwc_add_order_delivery_date_to_order ( $order_id ) {
+function dtwc_add_order_delivery_info_to_order ( $order_id ) {
 	if ( isset( $_POST ['dtwc_delivery_date'] ) && '' != $_POST ['dtwc_delivery_date'] ) {
 		add_post_meta( $order_id, 'dtwc_delivery_date',  sanitize_text_field( $_POST ['dtwc_delivery_date'] ) );
 	}
+	if ( isset( $_POST ['dtwc_delivery_time'] ) && '' != $_POST ['dtwc_delivery_time'] ) {
+		add_post_meta( $order_id, 'dtwc_delivery_time',  sanitize_text_field( $_POST ['dtwc_delivery_time'] ) );
+	}
 }
-add_action( 'woocommerce_checkout_update_order_meta', 'dtwc_add_order_delivery_date_to_order' , 10, 1 );
+add_action( 'woocommerce_checkout_update_order_meta', 'dtwc_add_order_delivery_info_to_order' , 10, 1 );
 
 /**
- * Add Delivery Date checkout field to WooCommerce emails.
+ * Add Delivery Date & Time checkout fields to WooCommerce emails.
  * 
  * @since 1.0
  */
-function dtwc_add_delivery_date_to_emails( $fields, $sent_to_admin, $order ) {
+function dtwc_add_delivery_info_to_emails( $fields, $sent_to_admin, $order ) {
     // Get the Order ID.
     if ( version_compare( get_option( 'woocommerce_version' ), '3.0.0', ">=" ) ) {
         $order_id = $order->get_id();
@@ -78,16 +101,28 @@ function dtwc_add_delivery_date_to_emails( $fields, $sent_to_admin, $order ) {
             'value' => $delivery_date,
         );
     }
+
+    // Get the delivery time.
+    $delivery_time = get_post_meta( $order_id, 'dtwc_delivery_time', true );
+
+    // Display delivery time.
+    if ( '' != $delivery_time ) {
+        $fields[ 'Delivery time' ] = array(
+            'label' => __( 'Delivery time', 'dtwc' ),
+            'value' => $delivery_time,
+        );
+    }
+
     return $fields;
 }
-add_filter( 'woocommerce_email_order_meta_fields', 'dtwc_add_delivery_date_to_emails' , 10, 3 );
+add_filter( 'woocommerce_email_order_meta_fields', 'dtwc_add_delivery_info_to_emails' , 10, 3 );
 
 /**
- * Add Delivery Date checkout field to WooCommerce thank you page.
+ * Add Delivery Date & Time checkout fields to WooCommerce thank you page.
  * 
  * @since 1.0
  */
-function dtwc_add_delivery_date_to_order_received_page( $order ) {
+function dtwc_add_delivery_info_to_order_received_page( $order ) {
     // Get the Order ID.
 	if ( version_compare( get_option( 'woocommerce_version' ), '3.0.0', ">=" ) ) {
         $order_id = $order->get_id();
@@ -101,10 +136,13 @@ function dtwc_add_delivery_date_to_order_received_page( $order ) {
     // Create readable delivery date.
     $delivery_date = date( 'M j, Y', strtotime( $delivery_date ) );
 
+    // Get the delivery time.
+    $delivery_time = get_post_meta( $order_id, 'dtwc_delivery_time', true );
+
     // Display the delivery date.
     if ( '' != $delivery_date ) {
-        echo '<p><strong>' . __( 'Delivery date', 'dtwc' ) . ':</strong> ' . $delivery_date . '</p>';
+        echo '<p><strong>' . __( 'Delivery date', 'dtwc' ) . ':</strong> ' . $delivery_date . ' @ ' . $delivery_time . '</p>';
 	}
 }
 //add_filter( 'woocommerce_order_details_before_order_table', 'dtwc_add_delivery_date_to_order_received_page', 10 , 1 );
-add_action( 'woocommerce_order_details_after_order_table_items', 'dtwc_add_delivery_date_to_order_received_page', 10 , 1 );
+add_action( 'woocommerce_order_details_after_order_table_items', 'dtwc_add_delivery_info_to_order_received_page', 10 , 1 );
