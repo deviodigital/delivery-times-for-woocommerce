feda391b9b90fb1dcaaba316041517cbe7487fa6
Robert DeVore <deviodigital@gmail.com>
2019-06-14 21:26:08 -0400

    :ok_hand: IMPROVE: Added new settings to datepicker

--- public/js/dtwc-public.js
+++ public/js/dtwc-public.js
@@ -1,6 +1,20 @@
 jQuery(document).ready(function( $ ) {
+	var deliveryDays = dtwc_settings.deliveryDays;
+	var weekDays = [ 'sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday' ];
+
 	$('#dtwc_delivery_date').datepicker( {
 		minDate: 0,
-        dateFormat: 'yy-mm-dd',
+		maxDate: dtwc_settings.maxDays,
+		showAnim: 'fadeIn',
+		dateFormat: 'yy-mm-dd',
+		firstDay: 0,
+		beforeShowDay: function(date) {
+			var currentWeekday = weekDays[ date.getDay() ];
+			if ( currentWeekday in deliveryDays ) {
+				// So enable the date here by returning an array
+				return [ true, "custom_css_class", "This date is available"];
+			}
+			return [ false, "unavailable_css_class", "This date is unavailable" ];
+		}
 	} );
 } );
