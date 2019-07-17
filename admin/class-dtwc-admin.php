b46d8f68871f08f4182ec5324cf121a4e577426d
Robert DeVore <deviodigital@gmail.com>
2019-07-16 21:04:46 -0400

    :ok_hand: IMPROVE: General code cleanup

--- admin/class-dtwc-admin.php
+++ admin/class-dtwc-admin.php
@@ -50,7 +50,7 @@ class DTWC_Admin {
 	public function __construct( $plugin_name, $version ) {
 
 		$this->plugin_name = $plugin_name;
-		$this->version = $version;
+		$this->version     = $version;
 
 	}
 
@@ -60,21 +60,7 @@ class DTWC_Admin {
 	 * @since    1.0
 	 */
 	public function enqueue_styles() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in DTWC_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The DTWC_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/dtwc-admin.css', array(), $this->version, 'all' );
-
+		//wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/dtwc-admin.css', array(), $this->version, 'all' );
 	}
 
 	/**
@@ -83,21 +69,7 @@ class DTWC_Admin {
 	 * @since    1.0
 	 */
 	public function enqueue_scripts() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in DTWC_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The DTWC_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/dtwc-admin.js', array( 'jquery' ), $this->version, false );
-
+		//wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/dtwc-admin.js', array( 'jquery' ), $this->version, false );
 	}
 
 }
